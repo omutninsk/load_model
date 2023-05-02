e7c4e41474df7643a8d3ed54df4bbbb7c4b5e46c
20142234 <ESGostyukhin@sberbank.ru>
2023-05-02 14:31:36 +0300

    chert js

---
+++ extractor/frontend/src/components/ChartView.vue
@@ -0,0 +1,18 @@
+import { Bar } from 'vue-chartjs'
+
+export default {
+    extends: Bar,
+    mounted () {
+        // Overwriting base render method with actual data.
+        this.renderChart({
+            labels: ['January', 'February', 'March'],
+            datasets: [
+                {
+                    label: 'GitHub Commits',
+                    backgroundColor: '#af6262',
+                    data: [40, 20, 12]
+                }
+            ]
+        })
+    }
+}
